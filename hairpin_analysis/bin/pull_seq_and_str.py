4fe78d170197ed40f8417b40527f05b5458cfd28
Jason Needham <jneedham@umd.edu>
2025-01-27 14:43:44 -0500

    Changed output to match new find_a_hairpin.py input

--- hairpin_analysis/bin/pull_seq_and_str.py
+++ hairpin_analysis/bin/pull_seq_and_str.py
@@ -3,30 +3,21 @@ import csv
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input", help = "Path to the input CSV file.") #
-parser.add_argument("-s", "--sequence", help = "Name of the sequence file.") #
-parser.add_argument("-d", "--dotbracket", help = "Name of the dotbracket file.") #
 parser.parse_args()
 args = parser.parse_args()
 
-if args.sequence:
-    seqFile = args.sequence
-else:
-    seqFile = "sequence"
-
-if args.dotbracket:
-    strFile = args.dotbracket
-else:
-    strFile = "sequence"
-
-with open(args.input, mode = 'r') as file:
-    csvFile = csv.reader(file)
-    counter = 0
-    for i in csvFile:
-        if counter > 0:
-            new_seq = seqFile+"_"+str(counter)+".seq"
-            new_str = strFile+"_"+str(counter)+".str"
-            with open(new_seq, 'w') as f:
-                f.write(i[9])
-            with open(new_str, 'w') as f:
-                f.write(i[10])
-        counter+=1
+if args.input:
+    try:
+        with open(args.input, mode = 'r') as file:
+            csvFile = csv.reader(file)
+            counter = 0
+            for i in csvFile:
+                if counter > 0:
+                    seq_name = i[0].replace(" ", "-")
+                    seq_name = seq_name.replace(",", "-")+"_"+str(counter)
+                    new_seq = seq_name+"\n"+i[9]+"\n"+i[10]
+                    with open(seq_name.replace(">", ""), 'w') as f:
+                        f.write(new_seq)
+                counter+=1
+    except:
+        print("Unable to pull data. Check the input file.")
